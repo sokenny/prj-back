733743a43fd301bae6d190ca09319e30ea6317f6
sokenny <juanchaher99@gmail.com>
2021-09-23 22:18:19 -0300

    fix

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json({limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
 // app.use(cors())
 app.use(cors({
-    origin: 'https://614d26c72e9d531e0f2e3d1f--fervent-lamport-727df5.netlify.app/'
+    origin: 'https://614d26c72e9d531e0f2e3d1f--fervent-lamport-727df5.netlify.app'
 }));
 
 // A esta ruta no le pasamos el middleware de verifyToken porque contiene la funcion "login" para la cual no se requiere el token
